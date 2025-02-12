f69d07447fc4b213fd7750e8aa7cf610f33616e9
piyushpal121 <rockybalboaa09@gmail.com>
2025-02-12 12:01:50 +0530

    done

--- Backend/fileSystem/fileasync.js
+++ Backend/fileSystem/fileasync.js
@@ -6,7 +6,11 @@ const fs=require('fs/promises');
 // read()
 
 const write = async () => {
+    try {
     const data = await fs.writeFile('./Backend/fileSystem/data.txt','This is new text!', 'utf-8');
-    console.log("Data Written => ", data)
+    console.log("Data Written => ", data)   
+    } catch (error) {
+    console.log(error);
+    }
 }
 write()
